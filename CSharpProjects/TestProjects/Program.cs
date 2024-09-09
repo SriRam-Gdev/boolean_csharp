9e48b6aa648a2a94ec1256708a1dfb639e5bdeba
Sriram s <123556950+SriRam-Gdev@users.noreply.github.com>
2024-09-09 14:51:32 +0530

    first commit

---
+++ CSharpProjects/TestProjects/Program.cs
@@ -0,0 +1,15 @@
+﻿switch (Fruits)
+{
+    case "Apple":
+        Console.WriteLine("Apple is my favorite fruit.");
+        break;
+    case "Banana":
+        Console.WriteLine("Banana is my favorite fruit.");
+        break;
+    case "Mango":
+        Console.WriteLine("Mango is my favorite fruit.");
+        break;
+    default:
+        Console.WriteLine("I don't like any other fruit.");
+        break;
+}
---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿Random coin = new Random();
+int flip = coin.Next(0,4);
+Console.WriteLine((flip == 1) ? "Heads" : "Tails");
+
+
+string access = "People|manager";
+int num = 50;
+if (access.Contains("manager"))
+{
+    if (num > 55)
+    {
+        Console.WriteLine("Access granted");
+    }
+    else
+    {
+        Console.WriteLine("Access denied");
+    }
+}
+else if (access.Contains("People"))
+{
+    if (num >= 20)
+    {
+        Console.WriteLine("Access granted");
+    }
+    else
+    {
+        Console.WriteLine("Access denied");
+    }
+}
+else
+{
+    Console.WriteLine("Access denied");
+}
+
+int[] numbers = { 4, 8, 15, 16, 23, 42 };
+int total = 0;
+bool found = true;
+
+foreach (int number in numbers)
+{
+    total += number;
+    if (number == 42)
+        found = true;
+}
+
+if (found)
+    Console.WriteLine("Set contains 42");
+
+Console.WriteLine($"Total: {total}");
+
